3b77bfe8c9dd664c3f75bf62330f384a4ea84f40
jonathan <jdavid598@hotmail.com>
2024-09-19 10:43:55 -0500

    correccion

--- src/sqs.service.consumer.ts
+++ src/sqs.service.consumer.ts
@@ -1,62 +1,72 @@
-import { DeleteMessageCommand, ReceiveMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { error } from "console";
-import { resolve } from "path";
+import { DeleteMessageCommand, ReceiveMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 @Injectable()
-export class SqsConsumer {
-    private sqsClient:SQSClient
-    private queueUrl: string = `https://sqs.us-east-1.amazonaws.com/376129868845/my-queue-app`
+export class SqsConsumer implements OnModuleInit {
+  private sqsClient: SQSClient;
+  private queueUrl: string = 'https://sqs.us-east-1.amazonaws.com/376129868845/my-queue-app';
 
-    constructor (){
-        this.sqsClient = new SQSClient({
-            region: 'us-east-1',
-        })
+  constructor() {
+    this.sqsClient = new SQSClient({
+      region: 'us-east-1',
+    });
+  }
+
+  async onModuleInit() {
+    console.log('SqsConsumer service is initializing...');
+    this.startPolling();
+  }
+
+  async startPolling() {
+    console.log('Starting to poll messages from SQS...');
+    while (true) {
+      await this.receiveMessages();
+      await new Promise(resolve => setTimeout(resolve, 5000));
     }
+  }
 
-    async onModuleInit() {
-         this.startPolling();
-    }
+  async receiveMessages() {
+    try {
+      console.log('Receiving messages...');
+      const command = new ReceiveMessageCommand({
+        QueueUrl: this.queueUrl,
+        MaxNumberOfMessages: 10,
+        WaitTimeSeconds: 20,
+      });
 
-    async startPolling(){
-        while (true){
-            await this.receivedMessages()
-            await new Promise(resolve => setTimeout(resolve, 1000))
+      const response = await this.sqsClient.send(command);
+
+      if (response.Messages) {
+        console.log(`Received ${response.Messages.length} messages.`);
+        for (const message of response.Messages) {
+          console.log('Processing message:', message.Body);
+          const messageBody = JSON.parse(message.Body || '{}');
+          await this.processMessage(messageBody);
+
+          const deleteCommand = new DeleteMessageCommand({
+            QueueUrl: this.queueUrl,
+            ReceiptHandle: message.ReceiptHandle,
+          });
+          await this.sqsClient.send(deleteCommand);
+          console.log('Deleted message:', message.ReceiptHandle);
         }
+      } else {
+        console.log('No messages to process.');
+      }
+    } catch (error) {
+      console.error('Error receiving messages:', error);
     }
+  }
 
-    async receivedMessages (){
-        try {
-            const command = new ReceiveMessageCommand({
-                QueueUrl: this.queueUrl,
-                MaxNumberOfMessages: 10,
-                WaitTimeSeconds:20,
-            })
+  async processMessage(messageBody: any) {
+    if (messageBody.action === 'create') {
+      console.log('Vehicle created:', messageBody.vehicle);
 
-            const response = await this.sqsClient.send(command);
-            if(response.Messages) {
-                for( const message of response.Messages){
-                    const messageBody = JSON.parse( message.Body || "{}" );
-                    await this.processMessage(messageBody);
+    } else if (messageBody.action === 'find') {
+      console.log('Vehicle found:', messageBody.vehicle);
 
-                    const deleteCommand = new DeleteMessageCommand({
-                        QueueUrl: this.queueUrl,
-                        ReceiptHandle: message.ReceiptHandle
-                    })
-                    await this.sqsClient.send(deleteCommand)
-                }
-            }
-
-        } catch( error) {
-            console.log("error receiving message:", error)
-        }
-    } 
-
-    async processMessage (messageBody: any){
-        if(messageBody.action === "create"){
-            console.log("vehicle created", messageBody.vehicle)
-        } else if (messageBody.action === "find") {
-            console.log("vehicle found", messageBody.vehicle)
-        }
+    } else {
+      console.warn('Unknown action:', messageBody.action);
     }
+  }
 }
